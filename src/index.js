ca1bb475ef5adfd078bfff46ab037d29bfdf2f94
Gerrit Lane <lane.gerrit@gmail.com>
2022-09-19 19:24:55 -0700

    Init folder structure and list post function

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const xss = require('xss');
+const app = express();
+const listItems = [];
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded( {extended: true }));
+
+app.get('/', (req, res) => {
+	res.render('index', {webListItems: listItems} );
+});
+
+app.post('/', (req, res) => {
+	const newListItem = xss(req.body.listItem);
+	console.log(newListItem);
+	listItems.push(newListItem);
+	res.render('index', {webListItems: listItems});
+});
+
+app.listen(3000, function() {
+	console.log('Server started on localhost port 3000');
+});
